78ca4432dda8e79e71e21aa157eabbb3f3aa0506
NoviceInDisguise <noviceindisguise@gmail.com>
2016-12-09 10:38:58 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,29 +1,22 @@
 import bpy
 
-#set convenient variable to current scene
-scn = bpy.context.scene
+scn = bpy.context.scene                 #set convenient variable to current scene
+sel = bpy.context.selected_sequences    #set convenient variable to selected sequences
 
-#check selected sequences
-sel_sequence = bpy.context.selected_sequences
+if len(sel) == 1:                           #if only one (1) sequence is selected, then execute
 
-if len(sel_sequence) == 1:
-    
-    track = sel_sequence[0]
-    
-    #get length of strip
-    str_len = track.frame_final_duration
-    
-    #cut track in half
-    scn.frame_current = str_len/2
-    bpy.ops.sequencer.cut(frame=(str_len/2),type='HARD',side='RIGHT')
-    
+    track = sel[0]                          #set track to the element in the array
+    str_len = track.frame_final_duration    #set variable to the length of selected sequence
+
+    bpy.ops.sequencer.cut(frame=(str_len/2)+1,type='HARD',side='RIGHT') #cut track in half
+
     #update selected
-    sel_sequence = bpy.context.selected_sequences
-    track = sel_sequence[0]
+    sel = bpy.context.selected_sequences    #reset sel to selected sequences
+    track = sel[0]                          #reset track to the element in the array
 
     #move track
-    track.channel = track.channel+1
-    track.frame_start = 1
+    track.channel = track.channel+1     #move right sequence up one channel
+    track.frame_start = 1               #move sequence to frame 1
 
     ############################
     #keyframe top track opacity#
@@ -33,21 +26,20 @@ if len(sel_sequence) == 1:
     data_str = data_str.split("scenes['Scene'].") #split string in to a list
     data_str = data_str[-1]                       #get last piece
     data_str = data_str[:-1]                      #remove last character
-    print(data_str)
-    
+    print("Selected sequence data path: " + str(data_str))
+
     #set to 1
     track.blend_alpha = 1                                                   #set alpha blend value
     scn.keyframe_insert(data_path=data_str + ".blend_alpha",frame=float(1)) #keyframe alpha blend value
-    #print("Keyframe set on frame 1")
+    print("Keyframe set on frame 1.0")
 
     #set to 0
     track.blend_alpha = 0                                                           #set alpha blend value
     scn.keyframe_insert(data_path=data_str + ".blend_alpha",frame=float(str_len/2)) #keyframe alpha blend value
-    #print("Keyframe set on frame " + str(str_len/2))
+    print("Keyframe set on frame " + str(str_len/2))
 
-    scn.frame_end = str_len/2       #set end frame to last frame
-
-    print(str_len)                  #print strip length
+    scn.frame_end = str_len/2                   #set end frame to last frame
+    print("End frame set to " + str(str_len/2)) #print strip length
 
 elif len(sel_sequence) > 1: 
     print("ERROR: Multiple sequences selected")
